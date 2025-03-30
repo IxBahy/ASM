2056d449d952baa5517c8d8b667f20abdc900308
IxBahy <ixbahy@gmail.com>
2025-03-30 04:40:47 +0200

    Add base Scanner to abstract common used functions

--- internal/scanners/scanner.go
+++ internal/scanners/scanner.go
@@ -1,6 +1,11 @@
 package scanners
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
 	"github.com/IxBahy/ASM/pkg/client"
 	"github.com/IxBahy/ASM/pkg/interfaces"
 )
@@ -9,6 +14,9 @@ type Scanner interface {
 	interfaces.Installable
 	Scan(target string) (ScannerResult, error)
 	GetConfig() ScannerConfig
+	GetInstallationState() InstallationState
+	IsInstalled() bool
+	RegisterInstallationStats() error
 }
 
 type ScannerConfig struct {
@@ -30,4 +38,51 @@ type ScannerResult struct {
 type InstallationState struct {
 	Installed bool
 	Version   string
+}
+
+type BaseScanner struct {
+	Config       ScannerConfig
+	InstallState InstallationState
+}
+
+func (s *BaseScanner) IsInstalled() bool {
+	if !s.InstallState.Installed {
+		if _, err := os.Stat(s.Config.ExecutablePath); err == nil {
+			s.RegisterInstallationStats()
+		} else if _, err := exec.LookPath(s.Config.Name); err == nil {
+			s.RegisterInstallationStats()
+		}
+	}
+
+	return s.InstallState.Installed
+}
+func (b *BaseScanner) GetConfig() ScannerConfig {
+	return b.Config
+}
+func (s *BaseScanner) RegisterInstallationStats() error {
+	s.InstallState.Installed = true
+
+	cmd := exec.Command(s.Config.Name, "--version")
+	output, err := cmd.CombinedOutput()
+	if err == nil {
+		lines := strings.Split(string(output), "\n")
+		for _, line := range lines {
+			if strings.Contains(line, "Version:") {
+				parts := strings.Split(line, ":")
+				if len(parts) > 1 {
+					s.InstallState.Version = strings.TrimSpace(parts[1])
+					break
+				}
+			}
+		}
+		if s.InstallState.Version == "" && len(lines) > 0 {
+			s.InstallState.Version = strings.TrimSpace(lines[0])
+		}
+	}
+
+	fmt.Printf("WPScan registered as installed, version: %s\n", s.InstallState.Version)
+	return nil
+}
+func (s *BaseScanner) GetInstallationState() InstallationState {
+	return s.InstallState
 }
